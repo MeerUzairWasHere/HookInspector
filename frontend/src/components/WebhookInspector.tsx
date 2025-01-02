bac8f3ffb5fd7f333dd60d7604baa837d9cfbc4c
Meer Uzair <meer.uxair007@gmail.com>
2025-01-03 01:26:02 +0530

    completed reqList

--- frontend/src/components/WebhookInspector.tsx
+++ frontend/src/components/WebhookInspector.tsx
@@ -5,25 +5,28 @@ import { Input } from "@/components/ui/input";
 import { Copy, Link } from "lucide-react";
 import { Request, RequestList } from "./RequestList";
 import { RequestDetails } from "./RequestDetails";
-import { MOCK_REQUESTS, WEBHOOK_URL } from "@/lib/constants";
 import { useWebhookUrl } from "@/lib/hooks/useWebhookUrl";
 import axios from "axios";
 
 export function WebhookInspector({ uuid }: { uuid: string }) {
   const { webhookUrl, copyUrl } = useWebhookUrl(uuid);
   const [requests, setRequests] = useState<Request[] | null>(null);
-
-  
-  console.log(webhookUrl);
+  const [selectedRequestId, setSelectedRequestId] = useState<number | null>(
+    null
+  );
   useEffect(() => {
     const getRequests = async () => {
       const response = await axios.get(webhookUrl + "/requests");
-      console.log(response);
-      // setRequests(data.requests);
+      setRequests(response.data);
     };
-
     getRequests();
   }, [uuid]);
+  console.log(requests);
+  
+  if (!requests) {
+    return null;
+  }
+  const selectedRequest = requests.find((r) => r.id === selectedRequestId);
 
   return (
     <div className="container mx-auto p-4 h-screen">
@@ -44,11 +47,11 @@ export function WebhookInspector({ uuid }: { uuid: string }) {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-[calc(100vh-12rem)]">
         <div className="border rounded-lg p-4">
           <h2 className="text-lg font-semibold mb-4">Requests</h2>
-          {/* <RequestList
-            requests={MOCK_REQUESTS}
+          <RequestList
+            requests={requests || []}
             selectedRequestId={selectedRequestId}
             onSelectRequest={setSelectedRequestId}
-          /> */}
+          />
         </div>
         <div className="border rounded-lg p-4">
           <h2 className="text-lg font-semibold mb-4">Details</h2>
