383de6fa1ec74e2da0ca9ebdd77c2976cf90dbfd
Meer Uzair <meer.uxair007@gmail.com>
2025-01-03 02:34:44 +0530

    wip

--- frontend/src/app/[id]/page.tsx
+++ frontend/src/app/[id]/page.tsx
@@ -1,7 +1,11 @@
 import { WebhookInspector } from "@/components/WebhookInspector";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = await params?.id; // Ensure params and id are accessible
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params; // Await params before accessing id
   if (!id) {
     throw new Error("ID is missing from the route parameters");
   }
